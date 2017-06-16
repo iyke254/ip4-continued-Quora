6b8ade94254f9b9068e6c7296811e055428ec520
iyke <nderiony95@gmail.com>
2017-06-16 16:09:13 +0300

    added dynamic segments

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    update(questions, params) {
-      this.sendAction('update', questions, params);
-    }
-  }
 });
