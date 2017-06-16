78a42c7d2d4a30491654c862be4e18a4643d53ff
iyke <nderiony95@gmail.com>
2017-06-16 16:02:18 +0300

    still on refactoring

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,11 +4,6 @@ export default Ember.Component.extend({
   actions: {
     update(questions, params) {
       this.sendAction('update', questions, params);
-    },
-    delete(questions) {
-      if (confirm('Are you sure you want to delete this question?')) {
-        this.sendAction('destroyQuestions', questions);
-      }
     }
   }
 });
