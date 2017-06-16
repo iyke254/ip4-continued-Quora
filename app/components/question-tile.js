eee2035b3ba13612d41b84eaf105aa67e2d088dc
iyke <nderiony95@gmail.com>
2017-06-16 14:30:23 +0300

    added the delete button and its functions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(questions) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('destroyQuestions', questions);
+      }
+    }
+  }
 });
--- app/index/route.js
+++ app/index/route.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('questions');
   },
+  actions: {
+    destroyQuestions(questions) {
+      questions.DestroyQuestions();
+      this.transitionTo('index');
+    }
+  }
 })
