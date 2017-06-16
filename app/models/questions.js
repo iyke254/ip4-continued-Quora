40befcb18173b676eee1e92e7ac2f67c91c556cf
iyke <nderiony95@gmail.com>
2017-06-16 17:09:11 +0300

    switching to one to many

--- app/models/questions.js
+++ app/models/questions.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   query: DS.attr(),
-  details: DS.attr()
+  details: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
