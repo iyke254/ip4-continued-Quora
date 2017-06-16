eafae8ec6533a187adc6b74205555f271ff6d20e
iyke <nderiony95@gmail.com>
2017-06-16 15:30:37 +0300

    added update form action handler

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionsForm() {
+      this.set('updateQuestionsForm', true);
+    },
+    update(questions) {
+      var params = {
+        name: this.get('name'),
+        query: this.get('query'),
+        details: this.get('details'),
+      };
+      this.set('updateQuestionsForm', false);
+      this.sendAction('update', questions, params);
+    }
+  }
 });
