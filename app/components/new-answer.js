f88c8b30d196b375fd91320b266e2ffcebaec07c
iyke <nderiony95@gmail.com>
2017-06-16 16:55:57 +0300

    add new answer action handler

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer',true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        reply: this.get('reply')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
