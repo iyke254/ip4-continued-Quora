badc4b468172712003b0673c053af13cd28ce530
iyke <nderiony95@gmail.com>
2017-06-19 08:33:16 +0300

    added a custom helper js

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
-export function questionPopularity(params/*, hash*/) {
-  return params;
+export function questionsPopularity(params) {
+  var questions = params[0];
+
+  if(questions.get('answers').get('length') >= 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  }
 }
 
-export default Ember.Helper.helper(questionPopularity);
+export default Ember.Helper.helper(questionsPopularity);
